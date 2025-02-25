a87e3ccab9c77d67456cbc590d685d6ca23c1063
Nitish singh <nitishkumarsingh877@gmail.com>
2025-02-25 11:32:14 +0530

    ✨ [Nitish] implemented lombok project in Employee payroll app

--- src/main/java/com/bridelabz/nitish/employeepayrollapp/dto/Employee.java
+++ src/main/java/com/bridelabz/nitish/employeepayrollapp/dto/Employee.java
@@ -4,9 +4,14 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
 
 @Entity
+@Data
+@NoArgsConstructor
 public class Employee {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -14,32 +19,9 @@ public class Employee {
     private String name;
     private double salary;
 
+
     public Employee(String name, double salary) {
         this.name = name;
-        this.salary = salary;
-    }
-    public Employee() {};
-
-    public Long getId() {
-        return id;
-    }
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public double getSalary() {
-        return salary;
-    }
-
-    public void setSalary(double salary) {
         this.salary = salary;
     }
 }

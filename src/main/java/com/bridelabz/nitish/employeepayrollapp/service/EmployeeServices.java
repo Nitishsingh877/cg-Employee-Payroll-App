ca1fe26d70bc90581e2aa0ed66acf8bb4a480449
Nitish singh <nitishkumarsingh877@gmail.com>
2025-02-27 11:51:37 +0530

    [Nitish] added various properties like department,gender,notes etc in payrollDTO and model

--- src/main/java/com/bridelabz/nitish/employeepayrollapp/service/EmployeeServices.java
+++ src/main/java/com/bridelabz/nitish/employeepayrollapp/service/EmployeeServices.java
@@ -1,13 +1,15 @@
 package com.bridelabz.nitish.employeepayrollapp.service;
 
-import com.bridelabz.nitish.employeepayrollapp.dto.Employee;
+import com.bridelabz.nitish.employeepayrollapp.dto.EmployeePayrollDTO;
+import com.bridelabz.nitish.employeepayrollapp.entities.EmployeePayrollData;
+
 
 import java.util.List;
 
 public interface EmployeeServices {
-    List<Employee> getEmployees();
-    Employee getEmployee(Long id);
-    Employee addEmployee(String name, double salary);
+    List<EmployeePayrollData> getEmployees();
+    EmployeePayrollDTO getEmployee(Long id);
+    EmployeePayrollDTO addEmployee(String name, double salary);
     void deleteEmployee(Long id);
-    Employee updateEmployeeSalary(Long id, double salary);
+    EmployeePayrollDTO updateEmployeeSalary(Long id, double salary);
 }
